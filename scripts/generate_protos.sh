cc85af5221a36b0f9f804346e9b508754731526b
samsonannan <samsonannan4@gmail.com>
2024-05-06 14:21:32 -0700

    format makefile v75

--- scripts/generate_protos.sh
+++ scripts/generate_protos.sh
@@ -5,8 +5,7 @@ export GOBIN=$(go env GOPATH)
 if ! command -v protoc-gen-go &> /dev/null; then
     echo "Warning: protoc-gen-go not installed"
     echo "installing protoc-gen-go"
-    echo $(go env)
-    go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+    sudo apt install -y protobuf-compilier
     if [ $? -ne 0 ]; then
         echo "Error: failed to install protoc-gen-go"
         exit 1
@@ -15,3 +14,4 @@ if ! command -v protoc-gen-go &> /dev/null; then
 fi
 
 protoc --version
+which protoc-gen-go-grpc
