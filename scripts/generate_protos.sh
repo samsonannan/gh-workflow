6c41660149d2830ba3e7418d1345a702a7799efc
samsonannan <samsonannan4@gmail.com>
2024-05-06 14:05:29 -0700

    format makefile v71

--- scripts/generate_protos.sh
+++ scripts/generate_protos.sh
@@ -5,6 +5,7 @@ export PATH=$PATH:$(go env GOPATH)/bin
 if ! command -v protoc-gen-go &> /dev/null; then
     echo "Warning: protoc-gen-go not installed"
     echo "installing protoc-gen-go"
+    echo $(go env)
     go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
     if [ $? -ne 0 ]; then
         echo "Error: failed to install protoc-gen-go"
